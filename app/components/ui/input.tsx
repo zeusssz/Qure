9735f627e574f4a8ff89ecd03544314326434511
zeus <136583387+zeusssz@users.noreply.github.com>
2024-12-16 18:04:37 +0530

    Update input.tsx

--- app/components/ui/input.tsx
+++ app/components/ui/input.tsx
@@ -1,5 +1,5 @@
 import * as React from "react"
-import { cn } from "@/app/lib/utils"
+import { cn } from "@/app/lib/utils/styles"
 
 export interface InputProps
   extends React.InputHTMLAttributes<HTMLInputElement> {}
