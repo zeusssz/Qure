07b7168b48c9955fbe3fc275db57206e8bed3e56
zeus <136583387+zeusssz@users.noreply.github.com>
2024-12-16 18:04:55 +0530

    Update checkbox.tsx

--- app/components/ui/checkbox.tsx
+++ app/components/ui/checkbox.tsx
@@ -3,7 +3,7 @@
 import * as React from "react"
 import * as CheckboxPrimitive from "@radix-ui/react-checkbox"
 import { Check } from "lucide-react"
-import { cn } from "@/app/lib/utils"
+import { cn } from "@/app/lib/utils/styles"
 
 const Checkbox = React.forwardRef<
   React.ElementRef<typeof CheckboxPrimitive.Root>,
