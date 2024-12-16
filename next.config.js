f46b751470364ed53ef4a0b6f4cb2c02abc7831a
SPS1010 <157950002+SPS1010@users.noreply.github.com>
2024-12-16 19:08:48 +0530

    Delete next.config.js

--- next.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  // Remove experimental.appDir as it's no longer needed in Next.js 14
-  swcMinify: true
-};
-
-module.exports = nextConfig;
